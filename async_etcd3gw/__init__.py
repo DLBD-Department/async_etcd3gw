9dba41da79dd8291b33e9c036b983ea3b598d292
Andrea Bonomi <andrea.bonomi@gmail.com>
2023-05-19 11:37:22 +0200

    version 0.8

--- async_etcd3gw/__init__.py
+++ async_etcd3gw/__init__.py
@@ -18,7 +18,7 @@ from .async_client import AsyncEtcd3Client, async_client
 from .async_lease import AsyncLease
 from .async_lock import AsyncLock
 
-__version__ = "0.7"
+__version__ = "0.8"
 
 __all__ = (
     "AsyncEtcd3Client",
